5bb67a0edbbf877846bd1cd99a8583eed72fdd37
Saahil <neon@saahild.com>
2025-08-10 11:39:45 -0400

    socket io

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-
+const http = require('http')
+const server = http.createServer(app);
+const io = require('socket.io')(server)
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -8,7 +10,11 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", { title: "Shipwrecked" });
 });
+io.on('connection', (socket) => {
+  // socket.emit 
+  // socket.on
 
-app.listen(3001, () => {
+})
+server.listen(3001, () => {
   console.log(`Server is running on http://localhost:3001`);
 });
